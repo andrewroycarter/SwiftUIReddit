c15a1fb096deff8d2b0cf0555e886c3227ae6701
Andrew Carter <andrew.carter@willowtreeapps.com>
2022-02-09 10:56:42 -0500

    Initial Commit

---
+++ SwiftUIReddit/SwiftUIRedditApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRedditApp.swift
+//  SwiftUIReddit
+//
+//  Created by Andrew Carter on 2/9/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRedditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
